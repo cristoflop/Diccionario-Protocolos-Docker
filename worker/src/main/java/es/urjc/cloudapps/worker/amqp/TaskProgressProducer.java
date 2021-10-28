0a685d2aa903541da004bd2c266eebf0b112680b
cristoflop <cristofer.lc.99@gmail.com>
2021-10-28 23:30:07 +0200

    Fix producing task progress message for consuming in server (worker)

--- worker/src/main/java/es/urjc/cloudapps/worker/amqp/TaskProgressProducer.java
+++ worker/src/main/java/es/urjc/cloudapps/worker/amqp/TaskProgressProducer.java
@@ -1,5 +1,7 @@
 package es.urjc.cloudapps.worker.amqp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import es.urjc.cloudapps.worker.dto.TaskProgressResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,16 +14,23 @@ public class TaskProgressProducer {
     private Logger log = LoggerFactory.getLogger(TaskProgressProducer.class);
 
     private final RabbitTemplate rt;
+    private final ObjectMapper om;
 
     private final String topic = "tasksProgress";
 
-    public TaskProgressProducer(RabbitTemplate rt) {
+    public TaskProgressProducer(RabbitTemplate rt, ObjectMapper om) {
         this.rt = rt;
+        this.om = om;
     }
 
     public void publishTaskProgress(TaskProgressResponse response) {
         log.info("Actualizacion de progresso: {}", response);
-        rt.convertAndSend(topic, response);
+        try {
+            String msg = om.writeValueAsString(response);
+            rt.convertAndSend(topic, msg);
+        } catch (JsonProcessingException e) {
+            log.info("El mensaje no se ha podido traducir bien a String y no se ha enviado.");
+        }
     }
 
 }
