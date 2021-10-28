01ec7cbc1d6c1ce9f3ca463bc792a1ed4dd72e68
cristoflop <cristofer.lc.99@gmail.com>
2021-10-28 21:22:01 +0200

    Disable testing TaskProgressConsumer (worker)

--- worker/src/main/java/es/urjc/cloudapps/worker/experimental/TaskProgressConsumer.java
+++ worker/src/main/java/es/urjc/cloudapps/worker/experimental/TaskProgressConsumer.java
@@ -4,9 +4,7 @@ import es.urjc.cloudapps.worker.dto.TaskProgressResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
 
-@Component
 public class TaskProgressConsumer {
 
     private final Logger log = LoggerFactory.getLogger(TaskProgressConsumer.class);
